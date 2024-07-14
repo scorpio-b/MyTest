362d3b4ff9535d937e6f80819b917ce2ab14e337
scorpio-b <scorpio_b@yeah.net>
2024-07-14 22:36:34 +0800

    打印结束语句

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Let's begin our test!" << std::endl;
+    std::cout << "It is the end of our test!" << std::endl;
     return 0;
 }
 
